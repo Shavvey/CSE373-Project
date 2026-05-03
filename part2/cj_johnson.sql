0ddda204344ed8eb40c7ae67ef89b6c323ba08ac
Cole Johnson <coledjohn@gmail.com>
2026-05-02 23:13:32 -0600

    Editing final coments

--- part2/cj_johnson.sql
+++ part2/cj_johnson.sql
@@ -20,11 +20,6 @@
 * I think dbms_output.put_line doesn't work in
 * interactive sessions or can't be used in triggers.
 *
-* - PROCEDURE 1: This procedure works, but only if
-* the past last name has no duplicate last names.
-* The INTO statement will raise an error if not.
-* I should have used a cursor here. 
-* I just ran out of time to fix this.
 **/
 
 -- set up spooling, if not already enabled
