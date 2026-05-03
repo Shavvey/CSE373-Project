c3a480b1da20fa3f8561839579776a1d54afd597
Cole Johnson <coledjohn@gmail.com>
2026-05-02 22:29:22 -0600

    Final comments

--- part2/cj_johnson.sql
+++ part2/cj_johnson.sql
@@ -6,6 +6,19 @@
 *
 * I certify that this assignment has been
 * completed by myself.
+*
+* == WHAT DOESN'T WORK =======================
+*
+* - INCLUSION DEPENDENCIES: There isn't a good
+* way to implement these in SQL. I tried using
+* update triggers, but it was buggy and enforce
+* particular insertion order. So, unfortunately
+* I didn't have a good solution for implementing
+* them into my schema. 
+*
+* - TRIGGER 2: I'm not sure why it's not working.
+* I think dbms_output.put_line doesn't work in
+* interactive sessions or can't be used in triggers.
 **/
 
 -- set up spooling, if not already enabled
@@ -32,6 +45,6 @@ SPOOL cj_johnson
 
 -- END: Cleanup by dropping all tables
 @cleanup
-
+-- epilog, turn off spool and echo
 SPOOL OFF
 set echo OFF
