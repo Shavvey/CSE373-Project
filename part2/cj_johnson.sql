cdb9f823fb702e36f5541d89d0cfff7b727dbcec
Cole Johnson <coledjohn@gmail.com>
2026-05-02 23:24:05 -0600

    Clean up table

--- part2/cj_johnson.sql
+++ part2/cj_johnson.sql
@@ -746,5 +746,48 @@ show errors
 execute flag_graduated_children
 SET SERVEROUTPUT OFF;
 
+-- FINAL STEP: cleanup 
+drop sequence id_seq -- drop automatic ID sequence
+;
+
+drop table member cascade constraints -- drop MEMBER
+;
+
+drop table school cascade constraints -- drop SCHOOL
+;
+ 
+drop table parent cascade constraints -- drop PARENT
+;
+
+drop table board_member cascade constraints -- drop BOARD MEMBER
+;
+
+drop table member_child cascade constraints -- drop MEMBER CHILD
+;
+
+drop table educator cascade constraints -- drop EDUCATOR
+;
+
+drop table volunteer_liason cascade constraints -- drop VOLUNTEER LIASON
+;
+
+drop table liason -- drop LIASON
+;
+
+drop table visitor -- drop VISITOR
+;
+
+drop table non_member cascade constraints -- drop NON MEMBER
+;
+
+drop table non_member_works_for cascade constraints -- drop NON MEMBER WORKS FOR
+;
+
+drop table non_member_child cascade constraints -- drop NON MEMBER CHILD
+;
+
+drop table meeting cascade constraints -- drop MEMBER
+;
+
 SPOOL OFF
 set echo OFF
